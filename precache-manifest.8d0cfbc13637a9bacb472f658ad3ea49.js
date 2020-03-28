9bd0fded7c5bfd9d92228b4eebb66972505ae55e
barjinderpaul <barjinder.singh@magicpin.in>
2020-03-28 23:34:12 +0530

    Updates

---
+++ precache-manifest.8d0cfbc13637a9bacb472f658ad3ea49.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f654af5bb5bad5616eb3b037f1c1030",
+    "url": "/contactify/index.html"
+  },
+  {
+    "revision": "72afa728af13282d5e47",
+    "url": "/contactify/static/css/2.ec4c23a0.chunk.css"
+  },
+  {
+    "revision": "2431321ddc23484e0ff9",
+    "url": "/contactify/static/css/main.3e205d4d.chunk.css"
+  },
+  {
+    "revision": "72afa728af13282d5e47",
+    "url": "/contactify/static/js/2.efb77689.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/contactify/static/js/2.efb77689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2431321ddc23484e0ff9",
+    "url": "/contactify/static/js/main.82f0934d.chunk.js"
+  },
+  {
+    "revision": "b5868ff4c75c16e97716",
+    "url": "/contactify/static/js/runtime-main.9f9b3cc7.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/contactify/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/contactify/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/contactify/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/contactify/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/contactify/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/contactify/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/contactify/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/contactify/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/contactify/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/contactify/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/contactify/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/contactify/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/contactify/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/contactify/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/contactify/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/contactify/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
